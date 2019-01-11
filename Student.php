a34d810951dd6cc243fa5386131a5a9fe072fb7c
William Bradshaw <wbradshaw4@learn.bcit.ca>
2019-01-11 12:20:55 -0800

    Added mutator methods to Student class

--- Student.php
+++ Student.php
@@ -19,5 +19,13 @@ class Student {
     $this->emails = array();
     $this->grades = array();
 }   
+
+function add_email($which,$address) {
+    $this->emails[$which] = $address;
+}
+
+function add_grade($grade) {
+    $this->grades[] = $grade;
+}
     
 }
